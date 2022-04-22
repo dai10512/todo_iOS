58c3a9202f9a3ef3d356d98982c45bc7eaffa698
Daisuke Osanai <dsk.osanai@gmail.com>
2022-04-22 19:59:23 +0900

    no comment

--- lib/main.dart
+++ lib/main.dart
@@ -42,34 +42,36 @@ class MyHomePage extends ConsumerWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Flexible(
-                    child: ListView.builder(
-                      itemCount: todoList.todos.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Dismissible(
-                          direction: DismissDirection.endToStart,
-                          key: Key(title),
-                          background: Container(
-                            padding: EdgeInsets.all(15),
-                            alignment: AlignmentDirectional.centerEnd,
-                            color: Colors.red,
-                            child: Icon(
-                              Icons.delete,
-                              color: Colors.white,
+                  Card(
+                    child: Flexible(
+                      child: ListView.builder(
+                        itemCount: todoList.todos.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Dismissible(
+                            direction: DismissDirection.endToStart,
+                            key: Key(title),
+                            background: Container(
+                              padding: const EdgeInsets.all(15),
+                              alignment: AlignmentDirectional.centerEnd,
+                              color: Colors.red,
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
                             ),
-                          ),
-                          onDismissed: (direction) {
-                            ref.read(todoListStateController.notifier).deleteTodo(todoList.todos[index].id.toString());
-                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('dismissed')));
-                            // print(todoList.todos.length);
-                          },
-                          child: Card(
-                            child: ListTile(
-                              title: Text(todoList.todos[index].title.toString()),
+                            onDismissed: (direction) {
+                              ref.read(todoListStateController.notifier).deleteTodo(todoList.todos[index].id.toString());
+                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('dismissed')));
+                              // print(todoList.todos.length);
+                            },
+                            child: Card(
+                              child: ListTile(
+                                title: Text(todoList.todos[index].title.toString()),
+                              ),
                             ),
-                          ),
-                        );
-                      },
+                          );
+                        },
+                      ),
                     ),
                   )
                 ],
@@ -100,7 +102,7 @@ Future aaa(context, ref, newTodoController) {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               width: MediaQuery.of(context).size.width * 0.6,
               child: TextFormField(
                 autovalidateMode: AutovalidateMode.onUserInteraction,
@@ -108,19 +110,22 @@ Future aaa(context, ref, newTodoController) {
                 decoration: const InputDecoration(labelText: "新規TODO"),
               ),
             ),
-            MaterialButton(
-              child: const Text('新規登録'),
-              onPressed: () {
-                var title = newTodoController.text;
-                ref.read(todoListStateController.notifier).add(title);
-                Navigator.of(context).pop();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("'$title'を新規登録しました"),
-                  ),
-                );
-                newTodoController.clear();
-              },
+            Container(
+              height: 20,
+              child: ElevatedButton(
+                child: const Text('新規登録'),
+                onPressed: () {
+                  var title = newTodoController.text;
+                  ref.read(todoListStateController.notifier).add(title);
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text("'$title'を新規登録しました"),
+                    ),
+                  );
+                  newTodoController.clear();
+                },
+              ),
             ),
           ],
         ),
